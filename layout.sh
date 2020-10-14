80b2b147c75c14d1ecb9d5a6b3619b8fd3584b1d
Tu Nombre <you@example.com>
2020-10-14 06:45:09 +0200

    Initital commit: initial layout done.

---
+++ layout.sh
@@ -0,0 +1,45 @@
+mkdir ci
+touch ci/__init__.py
+mkdir delivery
+touch delivery/__init__.py
+mkdir doc
+mkdir src
+touch src/__init__.py
+mkdir -p src/common/base
+mkdir -p src/common/datasets
+mkdir -p src/common/exception
+mkdir -p src/common/utils
+
+touch src/common/base/__init__.py
+touch src/common/datasets/__init__.py
+touch src/common/exception/__init__.py
+touch src/common/utils/__init__.py
+
+mkdir -p src/utils
+mkdir -p src/data
+mkdir -p src/process
+
+touch src/data/__init__.py
+touch src/process/__init__.py
+
+touch src/common/utils/decorators.py
+touch src/common/utils/validations.py
+touch src/common/conf.py
+touch src/common/constants.py
+touch src/common/singleton.py
+touch src/common/logger_configuration.py
+touch src/common/workspace.py
+touch src/utils/tools.py
+mkdir examples
+mkdir tests
+
+touch examples/__init__.py
+touch tests/__init__.py
+
+touch .gitignore
+touch README.md
+touch requirements.txt
+touch requirements_dev.txt
+touch setup.py
+touch VERSION
+
