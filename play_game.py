b4aa81c322b79c24649f78cda0a4962da4550779
Tu Nombre <you@example.com>
2020-10-19 06:53:58 +0200

    [REFACTOR] Cleaning code.

--- play_game.py
+++ play_game.py
@@ -1,16 +1,12 @@
 import seaborn as sns
 
-from pyabm.process.coordination_bep import play_coordination_bep
 from pyabm.process.run_population_game import play_population_game
-from pyabm.process.coordination_linear_dissatisfaction import play_coordination_linear_dissatisfaction
 
 sns.set(style="whitegrid")
 
 
 def main():
-    # play_coordination_bep()
     play_population_game()
-    # play_coordination_linear_dissatisfaction()
 
 
 if __name__ == "__main__":
